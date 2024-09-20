db12f7b48817faac70d6d0a47a733a2ab075ccd1
smizer09 <000libdio@gmail.com>
2024-09-20 21:38:40 +0300

    i added on page detail scraping and save the file to jason

--- bookscraping/bookscraping/spiders/bookspider.py
+++ bookscraping/bookscraping/spiders/bookspider.py
@@ -9,14 +9,20 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response):
         book = response.css('article.product_pod')
 
+        
         for book in book:
-            yield {
-                'name': book.css('h3 a::text').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a').attrib['href'],
-            }
+            relative_url = response.css('h3 a ::attr(href)').get()
+            if 'catalogue/' in relative_url:
+                book_url = "https://books.toscrape.com/" + relative_url 
+            else:
+                book_url = "https://books.toscrape.com/catalogue/" + relative_url
+
+            yield response.follow( book_url, callback =self.parse_book_page)
+
+
 
         next_page = response.css('li.next a::attr(href)').get()
+
 
         if next_page:
             if 'catalogue/' in next_page:
@@ -25,3 +31,23 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = "https://books.toscrape.com/catalogue/" + next_page
         
             yield response.follow( next_page_url, callback = self.parse)
+
+
+
+    def parse_book_page(self, response):
+        table_rows = response.css('table tr')
+        #for table_row in table_rows:
+        yield{
+                'url': response.url,
+                'title': response.css('.product_main h1::text').get(),
+                'product_type': table_rows[1].css("td ::text").get(),
+                'price_excl_tax': table_rows[2].css("td ::text").get(),
+                'price_incl_tax': table_rows[3].css("td ::text").get(),
+                'tax': table_rows[4].css("td ::text").get(),
+                'avaliablity': table_rows[5].css("td ::text").get(),
+                'num_reviews': table_rows[6].css("td ::text").get(),
+                'stars': response.css("p.star-rating").attrib['class'],
+                #'category': response.xpath("//ul[@class='breadcrumb']/li[@class='active]/preceding-sibling::li[1]/a/text()").get(),
+                'description': response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+                'price': response.css('p.price_color ::text').get(),
+            }
